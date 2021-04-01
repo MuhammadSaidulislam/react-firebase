55d0cd43114ba977bc80a07b9bb04606c8d08ffa
Saidul Islam <mislam171222@bscse.uiu.ac.bd>
2021-04-01 17:30:42 +0600

    firebase run

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDYT5N9nCgS_CAikj3bx3b0SIsIoyW6beQ",
+    authDomain: "react-firebase-9751f.firebaseapp.com",
+    projectId: "react-firebase-9751f",
+    storageBucket: "react-firebase-9751f.appspot.com",
+    messagingSenderId: "594379852502",
+    appId: "1:594379852502:web:cca962ae931775e4f65777",
+    measurementId: "G-9JTFWZWKPB"
+  };
+
+  export default firebaseConfig;
